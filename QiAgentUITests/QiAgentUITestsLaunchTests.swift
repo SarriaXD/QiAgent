a31cccfaa13e21695c469c2baf21f981fe83665b
SarriaXD <sarria.qi.wang@gmail.com>
2026-04-24 15:56:42 -0500

    Initial Commit

---
+++ QiAgentUITests/QiAgentUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  QiAgentUITestsLaunchTests.swift
+//  QiAgentUITests
+//
+//  Created by qi on 2026-04-24.
+//
+
+import XCTest
+
+final class QiAgentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
